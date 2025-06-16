9d48595ef76b664244f2704bf57163920e1dac45
nguetcheu <dnguetcheu@gmail.com>
2025-06-16 10:55:23 +0200

    Code de la classe de test

--- src/test/java/com/unitTest/Mockito/and/unitTest/Business/ListMockTest.java
+++ src/test/java/com/unitTest/Mockito/and/unitTest/Business/ListMockTest.java
@@ -1,2 +1,35 @@
-package com.unitTest.Mockito.and.unitTest.Business;public class ListMockTest {
+package com.unitTest.Mockito.and.unitTest.Business;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ListMockTest {
+
+    @Test
+    public void size_basic() {
+        List mock = mock(List.class);
+        when(mock.size()).thenReturn(5);
+        assertEquals(5, mock.size());
+    }
+
+    @Test
+    public void returnDifferentValues() {
+        List mock = mock(List.class);
+        when(mock.size()).thenReturn(5).thenReturn(10);
+        assertEquals(5, mock.size());
+        assertEquals(10, mock.size());
+    }
+
+    @Test
+    public void returnWithParameters() {
+        List mock = mock(List.class);
+        when(mock.get(0)).thenReturn("in28Minutes");
+        assertEquals("in28Minutes", mock.get(0));
+    }
 }
